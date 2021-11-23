c4dc7f7213698f6a5a07aa856d462736ff07230a
thiscouldbebetter <thiscouldbebetter@users.noreply.github.com>
2021-11-22 23:05:14 -0500

    Updated for latest version of Framework.

--- Source/main.js
+++ Source/main.js
@@ -3,6 +3,6 @@ function main() {
     var displaySize = Coords.fromXY(200, 200);
     var display = Display2D.fromSize(displaySize);
     var world = World2.random(displaySize);
-    Globals.Instance().initialize(200, // ticksPerSecond - Something's up, this is wrong.
+    Globals.Instance().initialize(200, // ticksPerSecond - Something's up, this is way off.
     display, world);
 }
